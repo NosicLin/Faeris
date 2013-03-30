69e672fcff509f6382a5f20fe327660136fd9f2a
NosicLin <nosic@faeris.com>
2013-03-30 13:36:09 +0800

    @add image resource

--- src/graphics/FsFontTTF.h
+++ src/graphics/FsFontTTF.h
@@ -4,14 +4,37 @@
 #include "mgr/FsResource.h"
 
 NS_FS_BEGIN
+
+
+class GlyphMetrices
+{
+	public:
+
+}
+class Glyph:public FsObject
+{
+	public:
+
+
+};
+
+
+class FontTTFData:public FsResource
+{
+
+};
+
+
 class FontTTF
 {
 	public:
-		FontTTF* create();
+		FontTTF* create(const char* name,int size);
 
 	private:
-
+		FontTTFData* m_data;
+		int m_size;
 };
+
 
 
 #endif /*_FS_FONT_TTF_H_*/
