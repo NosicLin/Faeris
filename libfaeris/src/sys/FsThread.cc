77da23393a978d073172f9d92f9872dee4371c9b
NosicLin <nosic@faeris.com>
2013-06-26 07:48:58 +0800

    @begin add thread,mutex,semaphore support

---
+++ libfaeris/src/sys/FsThread.cc
@@ -0,0 +1,82 @@
+#include "sys/FsThread.h"
+NS_FS_BEGIN
+static void* FsThread_Run(void* t)
+{
+	((Thread*)t)->run();
+	return NULL;
+}
+
+
+Thread::Thread()
+{
+	m_init=false;
+	memset(&m_tid,0,sizeof(pthread_t));
+}
+
+Thread::~Thread()
+{
+	/* thread is running, so we need  to stop it */
+	if(m_init)
+	{
+		pthread_cancel(m_tid);
+		m_init=false;
+	}
+}
+
+int Thread::start()
+{
+	if(m_init)
+	{
+		FS_TRACE_WARN("Thread Already Init");
+		return -1;
+	}
+
+	int ret=pthread_create(&m_tid,NULL,FsThread_Run,this);
+	if(ret<0)
+	{
+		FS_TRACE_WARN("Start Thread Failed");
+		ret=-1;
+	}
+	else
+	{
+		m_init=true;
+	}
+	return ret;
+}
+
+int Thread::join()
+{
+	if(!m_init)
+	{
+		FS_TRACE_WARN("Thread Not Init");
+		return -1;
+	}
+	int ret=pthread_join(m_tid,NULL);
+	if(ret<0)
+	{
+		ret=-1;
+		FS_TRACE_WARN("Join Thread Failed");
+	}
+	return ret;
+}
+
+int Thread::detach()
+{
+	if(!m_init)
+	{
+		FS_TRACE_WARN("Thread Not Init");
+		return -1;
+	}
+
+	int ret=pthread_detach(m_tid);
+	if(ret<0)
+	{
+		ret=-1;
+		FS_TRACE_WARN("Detach Thread Failed");
+	}
+	return ret;
+}
+
+NS_FS_END
+
+
